05f9d6147d732ce64b48f8b4ca1305cbe6551f33
Krish_Shah <1697krishshah@gmail.com>
2017-06-12 15:36:27 +0530

    Krish 12-6-17(1)

--- Medical_App/app/src/main/java/com/example/krish/medical_app/MainActivity.java
+++ Medical_App/app/src/main/java/com/example/krish/medical_app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.krish.medical_app;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,38 +16,12 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton more;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_patient);
-
-        more = (ImageButton) findViewById(R.id.imageButton_view_more);
-        more.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu menu = new PopupMenu(MainActivity.this, more);
-                menu.getMenuInflater().inflate(R.menu.patient_profile_options, menu.getMenu());
-
-                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        return true;
-                    }
-                });
-
-                menu.show();
-            }
-        });
-
+        setContentView(R.layout.my_patients);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.patient_profile_options, menu);
-        return true;
-    }
 }
+
